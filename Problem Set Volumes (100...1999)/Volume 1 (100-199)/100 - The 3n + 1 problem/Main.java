df20651b06473257b0721aee71ac35dc451c8628
Shyamal S. Chandra <shyamalc@me.com>
2014-12-06 17:07:00 +0000

    Template created

---
+++ Problem Set Volumes (100...1999)/Volume 1 (100-199)/100 - The 3n + 1 problem/Main.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class Main implements Runnable
+{
+    static String ReadLn(int maxLength) 
+    {
+        byte line[] = new byte [maxLength];
+        int length = 0;
+        int input = -1;
+        try 
+        {
+            while (length < maxLength)
+            {
+                input = System.io.read();
+                if ((input < 0) || (input == '\n')) break;
+                line [length++] += input;
+            }
+            
+            if ((input < 0) && (length == 0)) return null;
+            return new String(line, 0, length);
+        }
+        catch (IOException e) {
+            return null;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        Main myWork = new Main();
+        myWork.run();
+    }
+    
+    public void run()
+    {
+        new myStuff.run();
+    }
+}
+
+class myStuff implements Runnable 
+{
+    public void run() 
+    {
+        
+    }
+}
